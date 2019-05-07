d30cefffb43a964176186933038b22692db50e7b
Princy Agrawal <pa147267@igatenbcu.com>
2019-05-07 17:38:03 +0530

    first commit

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors=require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mongoRouter = require('./routes/mongoRouter');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/mongo', mongoRouter);
 app.use('/users', usersRouter);
 var mongoClient = require('mongodb').MongoClient;  //MongoDB driver
 
--- routes/index.js
+++ routes/index.js
@@ -1,111 +1,154 @@
 var express = require('express');
 const fs = require('fs');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
+var assert = require('assert');
 
+var db;
 
-/* GET home page. */
-router.get('/getAllUser', function (req, res, next) {
-  var data = fs.readFileSync('myJsonDB.json');
-  res.contentType('application/json');
-  res.send(data);
-});
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ "extended": false }));
 
-router.post('/regUser', function (req, res, next) {
-  var data = req.body;
+var server = "mongodb://localhost:27017"
+mongo.connect(server, function (err, response) {
+    if (err)
+        console.log(err);
 
-  let rawData = fs.readFileSync('myJsonDB.json', 'utf8');
-  var flag = 0;
-  console.log(rawData == '')
-  if (rawData !== '') {
-    console.log('<<<<<<<<<<<<', data)
-    parsedRawData = JSON.parse(rawData);
-    console.log('*********', parsedRawData.length)
-    for (var i = 0; i < parsedRawData.length; i++) {
-      console.log(parsedRawData[i].id)
-
-      console.log(data.id)
-      if (parsedRawData[i].id === data.id) {
-        flag = 1;
-        if (flag == 1) {
-          data = {
-            "status": "0",
-            "msg": "Already exist",
-
-          };
-
-        }
-      }
-      else {
-        // add the user here
-        rawData = []
-        console.log('<<<<<<<<<<<<<<<<<kkkkkk')
-        rawData.push(data);
-        fs.appendFile('myJsonDB.json', JSON.stringify(rawData) + ',', (err) => {
-          if(err) {
-            console.log('Error saving data')
-          } else {
-            console.log('User saved successfully')
-          }
-        });
-        data =
-          {
-            "status": "1",
-            "msg": "Inserted Success"
-          };
-      }
+    else {
+        console.log("Connection established successfully in Mongodb");
+           db= response.db('db_visitors');
+        // console.log('db -> ', db)
+        //db.collection("login").insertOne({userid:"princy",password:"1234"})
+        //  db.collection("items").insertOne(
+        //     { item: "canvas", qty: 100, tags: ["cotton"], size: { h: 28, w: 35.5, uom: "cm" } }
+        //  )
     }
-  } else {
-    console.log('>>>>>>>>>>>>>>>>>>', data)
-    rawData = [];
-    rawData.push(data);
-    fs.appendFile('myJsonDB.json', JSON.stringify(rawData) + ',', (err) => {
-      if(err) {
-        console.log('Error saving data')
-      } else {
-        console.log('User saved successfully')
-      }
-    });
-    data =
-      {
-        "status": "1",
-        "msg": "Inserted Success"
-      };
-
-  }
+    
+})
 
 
-  // if (rawData == null || rawData == '') {
-  //   console.log(rawData)
-  //   rawData = {
-  //     "data": []
-  //   };
-  // }
-  // else {
-  //   var parsedRawData = JSON.parse(rawData);
-  //   // console.log('%%%%%%%%%%%%')
-  //   // console.log(parsedRawData.length)
 
-  // }
-  // if (flag == 1) {
-  //   data = {
-  //     "status": "0",
-  //     "msg": "Already exist",
+router.post("/api/SaveUser", function (req, res, next) {
+    console.log('******************')
+    console.log(req.body)
+    console.log('******************')
+    // const visitor={
+    //     name:req.body.name,
+    //     visitorid:req.body.visitorid,
+    //     in_time:req.body.in_time,
+    //     comingfrom:req.body.comingfrom,
+    //     ssoid:req.body.ssoid,
+    //     authorizedname:req.body.authorizedname,
+    //     authorizedid:req.body.authorizedid
 
-  //   };
-  // }
+    // };
 
-  // else {
-  //    rawData.data.push(data);
-  //   fs.writeFileSync('myJsonDB.json', JSON.stringify(rawData.data));
-  //   data =
-  //     {
-  //       "status": "1",
-  //       "msg": "Inserted Success"
-  //     };
-  // }
-  res.send(data);
-});
+    // db.collection("details").insertOne(visitor,function(err,docs){
+    //    assert.equal(null,err);
+    //    console.log('item has been inserted');
+    //   res.send(docs);
+
+    // })
+    db.collection('details').find({ ssoid: req.body.ssoid }).toArray(function (err, results) {
+        console.log('666666666666666');
+        if (results.length > 0) {
+            console.log("already exist");
+            return res.status(400).json("Already exist");
+        }
+        else {
+            db.collection('details').insertOne(req.body, (err, result) => {
+                if (err) {
+                    return res.status(400).json(err);
+                } else {
+                    return res.status(201).json("saved to database");
+                }
+            })
+        }
+    })
+})
 
 
+router.get("/api/GetUser", function (req, res, next) {
+    db.collection("details").find({}).toArray(function (err, docs) {
+
+        if (err) {
+            return err;
+        } else {
+            res.send(docs);
+        }
+    })
+
+})
+router.post("/api/EditUser", function (req, res, next) {
+    let reqparameter = req.body;
+    console.log(reqparameter);
+    var myquery = { "ssoid": reqparameter.ssoid };
+    const update = { $set: { "name": reqparameter.name, "id": reqparameter.id, "time": reqparameter.time, "cf": reqparameter.cf, "authname": reqparameter.authname, "authid": reqparameter.authid } };
+    const options = { "upsert": false };
+    // // var newvalues={$set:{name:'',id:'',time:'',cf:'',authname:'',authid:''}};
+    //var newvalues={$set:{"ssoid":reqparameter.ssoid}};
+    db.collection("details").updateOne(myquery, update, function (err, docs) {
+        if (err) {
+            return res.status(400).json(err);
+        } else {
+            return res.status(201).json("updated");
+        }
+    })
+
+})
+router.post("/api/DeleteUser", function (req, res, next) {
+    let visitor = req.body;
+    var myquery = { 'ssoid': visitor.ssoid };
+
+    db.collection("details").deleteOne(myquery, function (err, docs) {
+        if (err) {
+            return res.status(400).json(err);
+        } else {
+            return res.status(201).json("deleted");
+        }
+    })
+
+})
+router.post("/api/Gettime", function (req, res, next) {
+    let visitor = req.body;
+    var myquery = { "ssoid": visitor.ssoid };
+    // const update={$set: { dateAdded: new Date()}};
+    const update = { $set: { dateAdded: Date() } };
+    const options = { "upsert": true };
+
+    //db.collection("details").insertOne( {$currentDate: {$type: "date"}},
+    //db.collection("details").insertOne( {outtime: { dateAdded: new Date() }},  { "upsert": true },
+    db.collection("details").updateOne(myquery, update,
+        function (err, docs) {
+            if (err) {
+                console.log(err)
+                return res.status(400).json(err);
+            } else {
+                return res.status(201).json("success");
+
+            }
+        });
+
+})
+router.get("/api/login",function(req,res,next){
+    username=req.body.userid;
+    password=req.body.password;
+    console.log("(((((((((");
+     var myquery={_id:username,password:password}
+     var output=db.collection("login").find(myquery)
+     if(output == username)
+       {
+           console.log("found")
+       }
+       else{
+           console.log("not found");
+       }
+})
 
 module.exports = router;
+
+
+
+
+
